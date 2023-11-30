01cc285f816efa6d83ea021c346c3ccccfe61ca9
Krishna47ak <anandakrishna720@gmail.com>
2023-12-01 01:32:43 +0530

    Fixes

--- src/app/(pages)/events/page.js
+++ src/app/(pages)/events/page.js
@@ -7,7 +7,7 @@ const Events = () => {
         <div className="bg-[url('/event-bg.jpg')] bg-cover bg-fixed pt-[9vh] bg-no-repeat max-w-10 min-h-[150vh] overflow-hidden" >
             <div className='grid md:grid-cols-2 lg:grid-cols-3 place-items-center md:gap-x-20 md:gap-y-5 md:px-16 lg:px-28 mt-10 md:mt-20' >
                 {eventsData?.map((event) => (
-                    <EventCard data={event} />
+                    <EventCard key={event?.desc} data={event} />
                 ))}
             </div>
         </div>
