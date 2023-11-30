0af2fbcfd83bd70d483419e6d0b46c0ab675a7c2
Krishna47ak <anandakrishna720@gmail.com>
2023-12-01 01:21:24 +0530

    Created about and events section in home screen

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import { backDropText } from "@/utils/contants"
+
+const About = () => {
+    // const truncate = (str) => {
+    //     return str.length > 10 ? str.substring(0, 7) + "..." : str;
+    // }
+
+    return (
+        <div className='relative text-white bg-gradient-to-r from-slate-900 rounded-t-3xl p-7 md:p-10 lg:px-20 pb-20' >
+            <div id="about" className='absolute -top-[9.5vh] left-0' />
+            <p className="text-3xl text-white text-center font-bold select-none mb-10" style={{ ...backDropText, fontSize: '2.5rem' }} >About</p>
+            <p className="text-justify" >
+                <span>
+                    Welcome to QuantumX-24, where creativity is sparked by curiosity and innovation meets inspiration! This is the highly anticipated return and second edition of the official tech event of New Horizon College of Engineering, Bengaluru. Our annual tech fair was founded with the goal of providing a platform that honours technology's spirit and encourages curiosity. Today, it serves as a guide for innovators, enthusiasts, and students alike.
+                </span>
+                <br />
+                <span className="hidden md:block" >
+                    At QuantumX, we think that technology has the ability to change the world and influence its course. Our festival serves as a hub for networking, knowledge exchange, and hands-on experiences that extend beyond academia's traditional boundaries. It's a place where ideas are born, prototypes are created, and lifelong connections are forged. Join us in this unique experience as we make the quantum leap into the future!
+                </span>
+            </p>
+
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/HomeEvents.js
@@ -0,0 +1,38 @@
+import { backDropText } from '@/utils/contants'
+import Link from 'next/link'
+import EventCard from './EventCard'
+import { byteBrawlDescImg, bestManagerDescImg, forumDescImg } from '@/utils/event-images'
+
+
+
+const HomeEvents = () => {
+    return (
+        <div className="relative bg-gradient-to-b from-[#0c0a0f] to-slate-900 p-10" >
+            <div id="events" className='absolute -top-[9vh] left-0' />
+            <p className="text-3xl text-white text-center font-bold select-none" style={{ ...backDropText, fontSize: '2.5rem' }} >Events</p>
+            <div className='grid md:grid-cols-3 place-items-center md:gap-x-20 md:px-28 mt-14' >
+                <EventCard data={{ img: byteBrawlDescImg }} from="home" />
+                <EventCard data={{ img: bestManagerDescImg }} from="home" />
+                <EventCard data={{ img: forumDescImg }} from="home" />
+            </div>
+            <Link href="/events" className='flex justify-center mt-12 mb-20 scale-90 md:scale-100' >
+                <div className="bg-[url('/btn-cyan.svg')] active:scale-95 bg-cover w-60 h-[3.1rem] bg-no-repeat flex items-center justify-center text-white text-lg font-mono font-semibold duration-200 z-10 cursor-pointer select-none" >
+                    Explore more
+                </div>
+            </Link>
+            <p className="text-3xl text-white text-center font-bold select-none mt-6" style={{ ...backDropText, fontSize: '2.5rem' }} >Workshops</p>
+            <div className='grid md:grid-cols-3 place-items-center md:gap-x-20 md:px-28 mt-16' >
+                <EventCard data={{ img: byteBrawlDescImg }} from="home" />
+                <EventCard data={{ img: bestManagerDescImg }} from="home" />
+                <EventCard data={{ img: forumDescImg }} from="home" />
+            </div>
+            <Link href="/workshops" className='flex justify-center mt-12 mb-20 scale-90 md:scale-100' >
+                <div className="bg-[url('/btn-cyan.svg')] active:scale-95 bg-cover w-60 h-[3.1rem] bg-no-repeat flex items-center justify-center text-white text-lg font-mono font-semibold duration-200 z-10 cursor-pointer select-none" >
+                    Explore more
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default HomeEvents
