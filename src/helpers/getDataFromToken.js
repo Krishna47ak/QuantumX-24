3b77127005227eef43f26bc177c99ec335e1570a
Krishna47ak <anandakrishna720@gmail.com>
2024-01-07 17:44:34 +0530

    Improvement

--- src/helpers/getDataFromToken.js
+++ src/helpers/getDataFromToken.js
@@ -4,9 +4,10 @@ export const getDataFromToken = (request) => {
     try {
         const token = request.cookies.get("token")?.value || '';
         const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-        return decodedToken.id;
+        return decodedToken?.id;
     } catch (error) {
-        throw new Error(error.message);
+        // throw new Error(error.message);
+        return null
     }
 
 }
