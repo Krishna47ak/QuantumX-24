21e38cf75a0fc735e64ad5e0792b62803c0fcfe2
Krishna47ak <anandakrishna720@gmail.com>
2024-01-17 19:56:47 +0530

    Updates and improvements in contact screen

--- src/app/(pages)/contact/page.js
+++ src/app/(pages)/contact/page.js
@@ -3,70 +3,110 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div className="bg-black bg-[url('/contact-bg.jpg')] p-20 text-white md:text-xl text-center pt-32 min-h-screen" >
+        <div className="bg-black bg-[url('/contact-bg.jpg')] py-16 md:py-20 px-7 -mr-5 text-white md:text-xl text-center min-h-screen overflow-hidden" >
             <br />
             <br />
-
-            <div className='grid lg:grid-cols-3' >
+            <div className='grid lg:grid-cols-2' >
                 <div>
-                    <div>
-                        <Image className='mx-auto' src="/sai-kiran.png" width={220} height={50} alt='sai kiran' />
-                        <p>saivostro2001@gmail.com</p>
-                        <p>+91 70222 67508</p>
-                        <p className='font-semibold text-purple-500 text-lg md:text-2xl' >Fest Lead</p>
+                    <div className='relative' >
+                        <Image className='mx-auto rounded-xl' src="/sai-contact.png" width={650} height={50} alt='aknair' />
+                        <div className='absolute z-10 left-[48%] bottom-[22.5%] min-[575px]:bottom-[20%] sm:bottom-[18.5%] text-[0.65rem] min-[450px]:text-xs min-[575px]:text-sm sm:text-lg md:text-xl lg:text-base xl:text-lg font-semibold space-y-[0.4rem] min-[450px]:space-y-3 md:space-y-4 lg:space-y-3 xl:space-y-4 text-left' >
+                            <p><a href='tel: +91 70222 67508' target='_blank'>+91 70222 67508</a></p>
+                            <p><a href='mailto: saivostro2001@gmail.com' target='_blank'>saivostro2001@gmail.com</a></p>
+                            <p><a href='https://www.instagram.com/its_sais_soch/' target='_blank'>@its_sais_soch</a></p>
+                        </div>
                         <br />
                     </div>
-                    <div className='lg:hidden' >
-                        <Image className='mx-auto scale-125 sm:scale-110' src="/ananda-krishna.png" width={400} height={50} alt='sai kiran' />
-                        <p>anandakrishna047@gmail.com</p>
-                        <p>+91 63623 98767</p>
-                        <p className='font-semibold text-purple-500 text-lg md:text-2xl' >Tech Lead</p>
+                    <div className='relative lg:hidden' >
+                        <Image className='mx-auto rounded-xl' src="/ananda-contact.png" unoptimized={true} priority width={650} height={50} alt='aknair' />
+                        <div className='absolute z-10 left-[48%] bottom-[22.5%] min-[575px]:bottom-[20%] sm:bottom-[18.5%] text-[0.65rem] min-[450px]:text-xs min-[575px]:text-sm sm:text-lg md:text-xl lg:text-base xl:text-lg font-semibold space-y-[0.4rem] min-[450px]:space-y-3 md:space-y-4 lg:space-y-3 xl:space-y-4 text-left' >
+                            <p><a href='tel: +91 63623 98767' target='_blank'>+91 63623 98767</a></p>
+                            <p><a href='mailto: anandakrishna047@gmail.com' target='_blank'>anandakrishna047@gmail.com</a></p>
+                            <p><a href='https://www.instagram.com/_ak_nair__/' target='_blank'>@_ak_nair__</a></p>
+                        </div>
                         <br />
                     </div>
-                    <div>
-                        <Image className='mx-auto' src="/mahesh-babu.png" width={300} height={50} alt='mahesh babu' />
-                        <p>maheshvejandla5@gmail.com</p>
-                        <p>+91 97018 48618</p>
-                        <p className='font-semibold text-purple-500 text-lg md:text-2xl' >Finance Lead</p>
+                    <div className='relative' >
+                        <Image className='mx-auto rounded-xl' src="/neha-contact.png" width={650} height={50} alt='aknair' />
+                        <div className='absolute z-10 left-[48%] bottom-[22.5%] min-[575px]:bottom-[20%] sm:bottom-[18.5%] text-[0.65rem] min-[450px]:text-xs min-[575px]:text-sm sm:text-lg md:text-xl lg:text-base xl:text-lg font-semibold space-y-[0.4rem] min-[450px]:space-y-3 md:space-y-4 lg:space-y-3 xl:space-y-4 text-left' >
+                            <p><a href='tel: +91 96068 20633' target='_blank'>+91 96068 20633</a></p>
+                            <p><a href='mailto: dneha750@gmail.com' target='_blank'>dneha750@gmail.com</a></p>
+                            <p><a href='https://www.instagram.com/_ak_nair__/' target='_blank'>@</a></p>
+                        </div>
                         <br />
                     </div>
-                    <div>
-                        <Image className='mx-auto' src="/neha-darawan.png" width={300} height={50} alt='neha darawan' />
-                        <p>dneha750@gmail.com</p>
-                        <p>+91 96068 20633</p>
-                        <p className='font-semibold text-purple-500 text-lg md:text-2xl' >Workshops Lead</p>
+                    <div className='relative' >
+                        <Image className='mx-auto rounded-xl' src="/adhithya-contact.png" width={650} height={50} alt='aknair' />
+                        <div className='absolute z-10 left-[50%] bottom-[22.5%] min-[575px]:bottom-[20%] sm:bottom-[17.5%] text-[0.65rem] min-[450px]:text-xs min-[575px]:text-sm sm:text-lg md:text-xl lg:text-base xl:text-lg font-semibold space-y-[0.4rem] min-[450px]:space-y-3 md:space-y-4 lg:space-y-3 xl:space-y-4 text-left' >
+                            <p><a href='tel: +91 79750 63417' target='_blank'>+91 79750 63417</a></p>
+                            <p><a href='mailto: adhithyaadhibn@gmail.com' target='_blank'>adhithyaadhibn@gmail.com</a></p>
+                            <p><a href='https://www.instagram.com/adhi.__.thya/' target='_blank'>@adhi.__.thya</a></p>
+                        </div>
+                        <br />
+                    </div>
+                    <div className='relative' >
+                        <Image className='mx-auto rounded-xl' src="/chetas-contact.png" width={650} height={50} alt='aknair' />
+                        <div className='absolute z-10 left-[48%] bottom-[22.5%] min-[575px]:bottom-[20%] sm:bottom-[18.5%] text-[0.65rem] min-[450px]:text-xs min-[575px]:text-sm sm:text-lg md:text-xl lg:text-base xl:text-lg font-semibold space-y-[0.4rem] min-[450px]:space-y-3 md:space-y-4 lg:space-y-3 xl:space-y-4 text-left' >
+                            <p><a href='tel: +91 88617 76508' target='_blank'>+91 88617 76508</a></p>
+                            <p><a href='mailto: anandakrishna047@gmail.com' target='_blank'>anandakrishna047@gmail.com</a></p>
+                            <p><a href='https://www.instagram.com/_ak_nair__/' target='_blank'>@</a></p>
+                        </div>
+                        <br />
+                    </div>
+                    <div className='relative' >
+                        <Image className='mx-auto rounded-xl' src="/ankita-contact.png" width={650} height={50} alt='aknair' />
+                        <div className='absolute z-10 left-[48%] bottom-[22.5%] min-[575px]:bottom-[20%] sm:bottom-[18.5%] text-[0.65rem] min-[450px]:text-xs min-[575px]:text-sm sm:text-lg md:text-xl lg:text-base xl:text-lg font-semibold space-y-[0.4rem] min-[450px]:space-y-3 md:space-y-4 lg:space-y-3 xl:space-y-4 text-left' >
+                            <p><a href='tel: +91 99011 78017' target='_blank'>+91 99011 78017</a></p>
+                            <p><a href='mailto: ankitashetti31@gmail.com' target='_blank'>ankitashetti31@gmail.com</a></p>
+                            <p><a href='https://www.instagram.com/_ak_nair__/' target='_blank'>@</a></p>
+                        </div>
                         <br />
                     </div>
                 </div>
-                <div className='hidden lg:flex justify-center items-center' >
-                    <div>
-                        <Image className='mx-auto' src="/ananda-krishna.png" width={400} height={50} alt='ananda krishna' />
-                        <p>anandakrishna047@gmail.com</p>
-                        <p>+91 63623 98767</p>
-                        <p className='font-semibold text-purple-500 text-lg md:text-2xl' >Tech Lead</p>
+                <div className='' >
+                    <div className='relative hidden lg:block' >
+                        <Image className='mx-auto rounded-xl' src="/ananda-contact.png" unoptimized={true} priority width={650} height={50} alt='aknair' />
+                        <div className='absolute z-10 left-[48%] bottom-[22.5%] min-[575px]:bottom-[20%] sm:bottom-[18.5%] text-[0.65rem] min-[450px]:text-xs min-[575px]:text-sm sm:text-lg md:text-xl lg:text-base xl:text-lg font-semibold space-y-[0.4rem] min-[450px]:space-y-3 md:space-y-4 lg:space-y-3 xl:space-y-4 text-left' >
+                            <p><a href='tel: +91 63623 98767' target='_blank'>+91 63623 98767</a></p>
+                            <p><a href='mailto: anandakrishna047@gmail.com' target='_blank'>anandakrishna047@gmail.com</a></p>
+                            <p><a href='https://www.instagram.com/_ak_nair__/' target='_blank'>@_ak_nair__</a></p>
+                        </div>
                         <br />
                     </div>
-                </div>
-                <div>
-                    <div>
-                        <Image className='mx-auto' src="/adhithya.png" width={300} height={50} alt='adhithya' />
-                        <p>adhithyaadhibn@gmail.com</p>
-                        <p>+91 79750 63417</p>
-                        <p className='font-semibold text-purple-500 text-lg md:text-2xl' >Creative Lead</p>
+                    <div className='relative' >
+                        <Image className='mx-auto rounded-xl' src="/mahesh-contact.png" width={650} height={50} alt='aknair' />
+                        <div className='absolute z-10 left-[50%] bottom-[22.5%] min-[575px]:bottom-[20%] sm:bottom-[17.5%] text-[0.65rem] min-[450px]:text-xs min-[575px]:text-sm sm:text-lg md:text-xl lg:text-base xl:text-lg font-semibold space-y-[0.4rem] min-[450px]:space-y-3 md:space-y-4 lg:space-y-3 xl:space-y-4 text-left' >
+                            <p><a href='tel: +91 97018 48618' target='_blank'>+91 97018 48618</a></p>
+                            <p><a href='mailto: maheshvejandla5@gmail.com' target='_blank'>maheshvejandla5@gmail.com</a></p>
+                            <p><a href='https://www.instagram.com/_ak_nair__/' target='_blank'>@</a></p>
+                        </div>
                         <br />
                     </div>
-                    <div>
-                        <Image className='mx-auto' src="/ankita-shetty.png" width={300} height={50} alt='sai kiran' />
-                        <p>ankitashetti31@gmail.com</p>
-                        <p>+91 99011 78017</p>
-                        <p className='font-semibold text-purple-500 text-lg md:text-2xl' >Hospitality Head</p>
+                    <div className='relative' >
+                        <Image className='mx-auto rounded-xl' src="/aamna-contact.png" width={650} height={50} alt='aknair' />
+                        <div className='absolute z-10 left-[50%] bottom-[22.5%] min-[575px]:bottom-[20%] sm:bottom-[17.5%] text-[0.65rem] min-[450px]:text-xs min-[575px]:text-sm sm:text-lg md:text-xl lg:text-base xl:text-lg font-semibold space-y-[0.4rem] min-[450px]:space-y-3 md:space-y-4 lg:space-y-3 xl:space-y-4 text-left' >
+                            <p><a href='tel: +91 95915 54317' target='_blank'>+91 95915 54317</a></p>
+                            <p><a href='mailto: anandakrishna047@gmail.com' target='_blank'>anandakrishna047@gmail.com</a></p>
+                            <p><a href='https://www.instagram.com/aamnanafiza7/' target='_blank'>@aamnanafiza7</a></p>
+                        </div>
                         <br />
                     </div>
-                    <div>
-                        <Image className='mx-auto' src="/abhijeeth-talari.png" width={300} height={50} alt='sai kiran' />
-                        <p>abhijeethtalari@gmail.com</p>
-                        <p>+91 86601 23179</p>
-                        <p className='font-semibold text-purple-500 text-lg md:text-2xl' >Events Lead</p>
+                    <div className='relative' >
+                        <Image className='mx-auto rounded-xl' src="/abhijeeth-contact.png" width={650} height={50} alt='aknair' />
+                        <div className='absolute z-10 left-[48%] bottom-[22.5%] min-[575px]:bottom-[20%] sm:bottom-[18.5%] text-[0.65rem] min-[450px]:text-xs min-[575px]:text-sm sm:text-lg md:text-xl lg:text-base xl:text-lg font-semibold space-y-[0.4rem] min-[450px]:space-y-3 md:space-y-4 lg:space-y-3 xl:space-y-4 text-left' >
+                            <p><a href='tel: +91 86601 23179' target='_blank'>+91 86601 23179</a></p>
+                            <p><a href='mailto: abhijeethtalari@gmail.com' target='_blank'>abhijeethtalari@gmail.com</a></p>
+                            <p><a href='https://www.instagram.com/_ak_nair__/' target='_blank'>@</a></p>
+                        </div>
+                        <br />
+                    </div>
+                    <div className='relative' >
+                        <Image className='mx-auto rounded-xl' src="/niharika-contact.png" width={650} height={50} alt='aknair' />
+                        <div className='absolute z-10 left-[48%] bottom-[22.5%] min-[575px]:bottom-[20%] sm:bottom-[18.5%] text-[0.65rem] min-[450px]:text-xs min-[575px]:text-sm sm:text-lg md:text-xl lg:text-base xl:text-lg font-semibold space-y-[0.4rem] min-[450px]:space-y-3 md:space-y-4 lg:space-y-3 xl:space-y-4 text-left' >
+                            <p><a href='tel: +91 98340 28369' target='_blank'>+91 98340 28369</a></p>
+                            <p><a href='mailto: anandakrishna047@gmail.com' target='_blank'>anandakrishna047@gmail.com</a></p>
+                            <p><a href='https://www.instagram.com/_ak_nair__/' target='_blank'>@</a></p>
+                        </div>
                         <br />
                     </div>
                 </div>
