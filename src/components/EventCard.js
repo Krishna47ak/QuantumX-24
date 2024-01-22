30a0903767dfa3f7b711740ccf52be51ebc6c7a7
Krishna47ak <anandakrishna720@gmail.com>
2024-01-22 14:44:13 +0530

    Updates

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -76,7 +76,7 @@ const EventCard = ({ data, from }) => {
                                     <div className='text-yellow-400 text-sm mt-2' >*Only for school students</div>
                                 )}
                             </div>
-                            {((data?.id != "QX_EV_04") && (data?.id != "QX_EV_05") && (data?.id != "QX_EV_06") && (data?.id != "QX_EV_07") && (data?.id != "QX_EV_14") && (data?.id != "QX_WS_26") && (data?.id != "QX_WS_09") && (data?.id != "QX_WS_21") && (data?.id != "QX_WS_15") && (data?.id != "QX_WS_17") && (data?.id != "QX_WS_19")) && (
+                            {((data?.id != "QX_EV_04") && (data?.id != "QX_EV_05") && (data?.id != "QX_EV_06") && (data?.id != "QX_EV_07") && (data?.id != "QX_EV_14") && (data?.id != "QX_EV_12") && (data?.id != "QX_WS_26") && (data?.id != "QX_WS_09") && (data?.id != "QX_WS_21") && (data?.id != "QX_WS_15") && (data?.id != "QX_WS_17") && (data?.id != "QX_WS_19")) && (
                                 <Link href={`${pathname}/register/${data?.id}`} className="bg-[url('/btn-yellow.svg')] active:scale-95 bg-cover min-w-60 w-60 min-h-[3.1rem] mt-5 bg-no-repeat flex items-center justify-center font-semibold duration-200 z-10 cursor-pointer select-none" >
                                     <p className='text-white text-lg font-mono' >Register</p>
                                 </Link>
