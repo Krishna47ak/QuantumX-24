8e7451e4a3401fc78237b2d6f13a23df81e712b5
Krishna47ak <anandakrishna720@gmail.com>
2024-10-03 21:11:07 +0530

    [ updated: volunteer form & quantumx logo ]

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className='bg-slate-950 p-6 sm:p-10' >
             <div className='bg-gradient-to-r from-slate-900 to-purple-950 md:grid md:grid-cols-2 md:place-items-center md:divide-x-2 divide-slate-700 p-7 rounded-xl' >
                 <div>
-                    <Image src="/logo-white.png" width={90} height={90} alt='logo' />
+                    <Image src="/logo-white2.png" width={90} height={90} alt='logo' />
                     <p className='text-white mt-4' >The Second Techno - Management fest of New Horizon College of Engineering.</p>
                     <div className='inline-flex mt-3 p-2 px-3 rounded-xl space-x-5 bg-gray-800' >
                         <a href='https://instagram.com/quantumx.nhce?igshid=OGQ5ZDc2ODk2ZA==' target='_blank' >
@@ -25,8 +25,8 @@ const Footer = () => {
                 </div>
                 <div className='md:pl-7 mt-5 md:mt-0' >
                     <p className='text-white text-3xl mb-4' >Contact Us</p>
-                    <p className='text-slate-500 text-lg' >QuantumX - 2024</p>
-                    <p className='text-slate-500 text-lg' >quantumx-24@newhorizonindia.edu</p>
+                    <p className='text-slate-500 text-lg' >QuantumX - 2025</p>
+                    {/* <p className='text-slate-500 text-lg' >quantumx-24@newhorizonindia.edu</p> */}
                 </div>
             </div>
         </div>
